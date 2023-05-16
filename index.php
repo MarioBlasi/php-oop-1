c1d2516b935f2e14c4dbf178681f71d7e4f0fbae
MarioBlasi <mblasi509@gmail.com>
2023-05-16 14:19:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>php-oop-1</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main class="container mt-5 shadow text-center">
+  <?php
+        class Fruit {
+        // Properties
+        public $name;
+        public $color;
+
+        // Methods
+        function set_name($name) {
+            $this->name = $name;
+        }
+        function get_name() {
+            return $this->name;
+        }
+        function set_color($color) {
+            $this->color = $color;
+        }
+        function get_color() {
+            return $this->color;
+        }
+}
+
+        $apple = new Fruit();
+        $apple->set_name('Apple');
+        $apple->set_color('Red');
+        echo "Name: " . $apple->get_name();
+        echo "<br>";
+        echo "Color: " .  $apple->get_color();
+
+        var_dump($apple);
+?>
+
+  </main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
