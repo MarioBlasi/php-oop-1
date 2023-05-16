a05e6e9fc0c2206755c33acaee47714bbc61a722
MarioBlasi <mblasi509@gmail.com>
2023-05-16 15:40:04 +0100

    layout style

--- index.php
+++ index.php
@@ -15,9 +15,13 @@
 
 <body>
   <header>
-    <!-- place navbar here -->
+    <div class="container">
+        <h1 class=" text-danger shadow mt-3 text-center ">
+            PHP OOP!
+        </h1>
+    </div>
   </header>
-  <main class="container mt-5 shadow text-center">
+  <main class="container mt-4 shadow text-center">
 
     <?php
 
@@ -55,28 +59,55 @@
         public function getGenre() {
             return $this->genre;
         }
+
+
     }
 
     // Creazione degli oggetti Genre
     $genre1 = new Genre("Commedia");
     $genre2 = new Genre("Azione");
+    $genre3 = new Genre("Fantasy");
+    $genre4 = new Genre("Horror");
+    $genre5 = new Genre("Sci-fi");
 
     // Creazione degli oggetti Movie
     $movie1 = new Movie("Film 1", 2020, $genre1);
     $movie2 = new Movie("Film 2", 2018, $genre2);
+    $movie3 = new Movie("Film 3", 2008, $genre3);
+    $movie4 = new Movie("Film 4", 2088, $genre4);
+    $movie5 = new Movie("Film 5", 2118, $genre5);
 
     ?>
-
-    <h2>Film 1:</h2>
-    <p>Titolo: <?php echo $movie1->getTitle(); ?></p>
-    <p>Anno: <?php echo $movie1->getYear(); ?></p>
-    <p>Genere: <?php echo $movie1->getGenre()->getName(); ?></p>
-
-    <h2>Film 2:</h2>
-    <p>Titolo: <?php echo $movie2->getTitle(); ?></p>
-    <p>Anno: <?php echo $movie2->getYear(); ?></p>
-    <p>Genere: <?php echo $movie2->getGenre()->getName(); ?></p>
-
+    <div class="shadow">
+        <h4 class="bg-info">Film 1:</h4>
+        <p>Titolo: <?php echo $movie1->getTitle(); ?></p>
+        <p>Anno: <?php echo $movie1->getYear(); ?></p>
+        <p>Genere: <?php echo $movie1->getGenre()->getName(); ?></p>
+    </div>
+    <div class="shadow">
+        <h4 class="bg-info">Film 2:</h4>
+        <p>Titolo: <?php echo $movie2->getTitle(); ?></p>
+        <p>Anno: <?php echo $movie2->getYear(); ?></p>
+        <p>Genere: <?php echo $movie2->getGenre()->getName(); ?></p>
+    </div>
+    <div class="shadow">
+        <h4 class="bg-info">Film 3:</h4>
+        <p>Titolo: <?php echo $movie3->getTitle(); ?></p>
+        <p>Anno: <?php echo $movie3->getYear(); ?></p>
+        <p>Genere: <?php echo $movie3->getGenre()->getName(); ?></p>
+    </div>
+    <div class="shadow">
+        <h4 class="bg-info">Film 4:</h4>
+        <p>Titolo: <?php echo $movie4->getTitle(); ?></p>
+        <p>Anno: <?php echo $movie4->getYear(); ?></p>
+        <p>Genere: <?php echo $movie4->getGenre()->getName(); ?></p>
+    </div>
+    <div class="shadow">
+        <h4 class="bg-info">Film 5:</h4>
+        <p>Titolo: <?php echo $movie5->getTitle(); ?></p>
+        <p>Anno: <?php echo $movie5->getYear(); ?></p>
+        <p>Genere: <?php echo $movie5->getGenre()->getName(); ?></p>
+    </div>
   </main>
   <footer>
     <!-- place footer here -->
